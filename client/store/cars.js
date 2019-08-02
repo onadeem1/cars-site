97faaeb1955670d546608f34b004733bf4b701b0
Omer Nadeem <omernadeem3@gmail.com>
2019-08-02 12:02:22 -0400

    feat(client): add actions to cars reducer to update, remove, etc

--- client/store/cars.js
+++ client/store/cars.js
@@ -1,47 +1,92 @@
 import axios from 'axios'
 import history from '../history'
 
-/* ACTION TYPES */
+/* Action Types */
 const ADD_CAR = 'ADD_CAR'
+const SELECT_CAR = 'SELECT_CAR'
+const UPDATE_CAR = 'UPDATE_CAR'
+const REMOVE_CAR = 'REMOVE_CAR'
 
-/* ACTION CREATORS */
+/* Action Creators */
 export const addCar = car => ({type: ADD_CAR, car})
-
-/* REDUCER */
-export default function(state = [], action) {
-  switch (action.type) {
-    case ADD_CAR:
-      return [...state, action.car]
-    default:
-      return state
-  }
+export const selectCar = carKey => ({type: SELECT_CAR, carKey})
+export const updateCar = car => {
+  return {type: UPDATE_CAR, car}
 }
+export const removeCar = carKey => ({type: REMOVE_CAR, carKey})
 
-/* THUNK CREATORS EXTRA FOR NOW */
-
-export const finalSubmit = (user, cars) => async dispatch => {
+/* Thunk Creators */
+export const finalSubmit = (user, cars) => async () => {
   let res
   try {
     res = await axios.post(`/api/cars`, {user, cars})
     if (res.status === 201) history.push('/thanks')
-    console.log(res)
   } catch (error) {
     console.error(error)
   }
 }
 
-// export const auth = (email, password, method) => async dispatch => {
-//   let res
-//   try {
-//     res = await axios.post(`/auth/${method}`, {email, password})
-//   } catch (authError) {
-//     return dispatch(getUser({error: authError}))
-//   }
+/* Initial State */
+const initialState = {
+  selected: {},
+  cars: [
+    {
+      make: 'Nissan',
+      model: 'Altima',
+      minYear: 1999,
+      maxYear: 2007,
+      minBudget: 2000,
+      maxBudget: 5000,
+      maxMileage: 25000,
+      zip: 11801,
+      carKey: 'lksdjflkjdslfjds'
+    },
+    {
+      make: 'Nissan',
+      model: 'sentraa',
+      minYear: 1999,
+      maxYear: 2007,
+      minBudget: 2000,
+      maxBudget: 5000,
+      maxMileage: 25000,
+      zip: 10003,
+      carKey: 'jdjdjdjdjdjdkjdk'
+    }
+  ]
+}
 
-//   try {
-//     dispatch(getUser(res.data))
-//     history.push('/home')
-//   } catch (dispatchOrHistoryErr) {
-//     console.error(dispatchOrHistoryErr)
-//   }
-// }
+/* Reducer */
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case ADD_CAR:
+      return {...state, cars: [...state.cars, action.car]}
+
+    case SELECT_CAR:
+      return {
+        ...state,
+        selected: state.cars.filter(car => car.carKey === action.carKey)[0]
+      }
+
+    case REMOVE_CAR:
+      return {
+        ...state,
+        cars: state.cars.filter(car => {
+          if (car.carKey === action.carKey) return false
+          else return true
+        })
+      }
+
+    case UPDATE_CAR:
+      return {
+        ...state,
+        cars: state.cars.map(car => {
+          if (car.carKey === action.car.carKey) {
+            return {...car, ...action.car}
+          } else return car
+        }),
+        selected: {}
+      }
+    default:
+      return state
+  }
+}
