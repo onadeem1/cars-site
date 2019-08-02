e4d48827be6b1654d425e0686eacdefd63661a78
Omer Nadeem <omernadeem3@gmail.com>
2019-08-02 11:54:47 -0400

    feat(client): add routing for updates & admin

--- client/routes.js
+++ client/routes.js
@@ -1,18 +1,24 @@
 import React from 'react'
 import {Route, Switch} from 'react-router-dom'
-import CarForm from './components/form'
+
+import CarForm from './components/form/car-form'
 import Summary from './components/form/summary'
 import UserForm from './components/form/user-form'
 import Thanks from './components/form/thanks'
+import Auth from './components/admin'
 
-const Routes = () => (
-  <Switch>
-    <Route exact path="/" component={CarForm} />
-    <Route path="/summary" component={Summary} />
-    <Route path="/user" component={UserForm} />
-    <Route path="/thanks" component={Thanks} />
-    <Route component={CarForm} />
-  </Switch>
-)
+const Routes = () => {
+  return (
+    <Switch>
+      <Route exact path="/" component={CarForm} />
+      <Route path="/summary" component={Summary} />
+      <Route path="/user" component={UserForm} />
+      <Route path="/thanks" component={Thanks} />
+      <Route path="/update" component={CarForm} />
+      <Route path="/admin" component={Auth} />
+      <Route component={CarForm} />
+    </Switch>
+  )
+}
 
 export default Routes
