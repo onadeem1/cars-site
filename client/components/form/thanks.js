cec7f778da0156042035c6d23e96da458ec5626b
Omer Nadeem <omernadeem3@gmail.com>
2019-08-02 12:00:26 -0400

    feat(client): create thank you screen after final submit

--- client/components/form/thanks.js
+++ client/components/form/thanks.js
@@ -1,5 +1,29 @@
-import React, {useState} from 'react'
+import React from 'react'
+import carImage from '../../images/car-background.jpeg'
+import checkImage from '../../images/check-mark.png'
+import './css/thanks.css'
 
-const App = () => <div>Thank You</div>
+const App = () => (
+  <div className="thanks-page">
+    <img className="car-background" src={carImage} />
+    <div className="thanks-page-container">
+      <div className="check-container">
+        <img className="check-image" src={checkImage} />
+      </div>
+      <div className="confirmed-text-container">
+        <div className="confirmed-text">Confirmed!</div>
+        <div className="confirmed-details-text">
+          We got your request, we'll send you an email<br />when we find a
+          match.
+        </div>
+      </div>
+      <div className="thanks-button-container">
+        <button className="next-btn" type="submit">
+          Thank You!
+        </button>
+      </div>
+    </div>
+  </div>
+)
 
 export default App
