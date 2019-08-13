0e4c4ee97863f877f85042395580f986bace8a6f
Omer Nadeem <omernadeem3@gmail.com>
2019-08-13 15:47:42 -0400

    feat(client): add header image

--- client/app.js
+++ client/app.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Routes from './routes'
 import carImage from './images/car-background.jpeg'
+import Header from './components/header'
 import './components/styling/app.scss'
 
 const App = () => {
   return (
     <div className="app-container">
+      <Header />
       <img className="car-background" src={carImage} />
       <Routes />
     </div>
---
+++ client/components/header/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import header from '../../images/header.png'
+
+const Header = () => (
+  <div className="header-image-container">
+    <img className="header-image" src={header} />
+  </div>
+)
+
+export default Header
